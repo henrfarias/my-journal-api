5a45434c972967d0365756eb1fba01d4043eb88c
Henrique Farias <henrique.biasibetti@gmail.com>
2023-02-04 12:50:50 -0300

    feat: add entities interface
    Signed-off-by: Henrique Farias <henrique.biasibetti@gmail.com>

---
+++ src/domain/entity/tag.ts
@@ -0,0 +1,5 @@
+export interface Tag {
+  name: string
+  authorId: string
+  hex_color: string
+}
---
+++ src/domain/entity/thought.ts
@@ -0,0 +1,5 @@
+export interface Thought {
+  body: string
+  authorId: string
+  tagId?: string
+}
